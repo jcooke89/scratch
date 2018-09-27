92ca518e1e9070812a6201553de14b882a8d8119
james cooke <james.cooke@just-eat.com>
2018-09-27 19:23:16 +0100

    broker chain pattern

---
+++ brokerChain/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace brokerChain
+{
+    public class Game
+    {
+        public event EventHandler<Query> Queries;
+        public void PerformQuery(object sender, Query q)
+        {
+            Queries?.Invoke(sender, q);
+        }
+    }
+    public class Query
+    {
+        public string CreatureName;
+
+        public enum Argument
+        {
+            Attack, Defense
+        }
+
+        public Argument WhatToQuery;
+        public int Value;
+        public Query(string creatureName, Argument whatToQuery, int value)
+        {
+            CreatureName = creatureName;
+            WhatToQuery = whatToQuery;
+            Value = value;
+        }
+    }
+
+    public class Creature
+    {
+        private Game game;
+        public string Name;
+        private int defense;
+        private int attack;
+
+        public Creature(Game game, string name, int defense, int attack)
+        {
+            this.game = game;
+            this.Name = name;
+            this.defense = defense;
+            this.attack = attack;
+        }
+
+        public int Attack
+        {
+            get
+            {
+                var q = new Query(Name, Query.Argument.Attack, attack);
+                game.PerformQuery(this, q);
+                return q.Value;
+            }
+        }
+        public int Defense
+        {
+            get
+            {
+                var q = new Query(Name, Query.Argument.Defense, attack);
+                game.PerformQuery(this, q);
+                return q.Value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Attack)}: {Attack}, {nameof(Defense)}: {Defense},";
+        }
+    }
+
+    public abstract class CreatureModifier : IDisposable
+    {
+        protected Game game;
+        protected Creature creature;
+        public CreatureModifier(Game game, Creature creature)
+        {
+            this.game = game;
+            this.creature = creature;
+            game.Queries += Handle;
+        }
+        protected abstract void Handle(object sender, Query q);
+        public void Dispose()
+        {
+            game.Queries -= Handle;
+        }
+    }
+
+    public class DoubleAttackModifier : CreatureModifier
+    {
+        public DoubleAttackModifier(Game game, Creature creature) : base(game, creature) { }
+
+        protected override void Handle(object sender, Query q)
+        {
+            if (q.CreatureName == this.creature.Name
+            && q.WhatToQuery == Query.Argument.Attack)
+            {
+                q.Value *= 2;
+            }
+        }
+    }
+    public class DoubleDefenseModifier : CreatureModifier
+    {
+        public DoubleDefenseModifier(Game game, Creature creature) : base(game, creature) { }
+
+        protected override void Handle(object sender, Query q)
+        {
+            if (q.CreatureName == this.creature.Name
+            && q.WhatToQuery == Query.Argument.Defense)
+            {
+                q.Value *= 2;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            var someOrribleThing = new Creature(game, "some 'orrible thing", 3, 3);
+
+            Console.WriteLine(someOrribleThing);
+
+            using(new DoubleAttackModifier(game, someOrribleThing)){
+                Console.WriteLine(someOrribleThing);
+                using(new DoubleDefenseModifier(game, someOrribleThing)){
+                    Console.WriteLine(someOrribleThing);
+                }
+            }
+
+            Console.WriteLine(someOrribleThing);
+        }
+    }
+}
